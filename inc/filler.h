91c92badc720e5c917d28b2dcc1f7cdcbb13d4b4
Andrew GALAVAN <agalavan@student.unit.ua>
2018-02-23 16:24:35 +0200

    header added

--- inc/filler.h
+++ inc/filler.h
@@ -32,4 +32,13 @@ typedef struct	s_board
 
 t_game_elem		*g_filler;
 
+void	fill_token_with_figure(int fd);
+void	fill_board_with_lines(int fd);
+void	read_input_and_fill_struct(int fd);
+
+void	find_best_move(void);
+void	find_opponent(char ch);
+int		check_neighbors(int y, int x);
+void	surround_with_numbers(int y, int x, int i);
+
 #endif
